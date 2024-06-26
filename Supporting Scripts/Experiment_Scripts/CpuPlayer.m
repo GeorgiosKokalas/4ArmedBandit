9ff98ea660bc23fe11c1470979a9cb164bfea641
aniluchavez <anilu.chavez@bcm.edu>
2024-06-26 10:30:29 -0500

    cpu changes

--- Supporting Scripts/Experiment_Scripts/CpuPlayer.m
+++ Supporting Scripts/Experiment_Scripts/CpuPlayer.m
@@ -10,6 +10,9 @@ classdef CpuPlayer < handle
         Choice_List     % The list of choices that we have   
         Next_Choice     % The choice that will be made next by the Cpu
         Choice_Origins  % The start choice that we define
+        Epsilon         % Sets the epsilon value for e-greedy algo
+        Rewards         % Rewards received
+        Counts          % Counts per choice
     end
 
     methods
@@ -18,35 +21,63 @@ classdef CpuPlayer < handle
             if ~exist("behavior_mode", "var"); behavior_mode = 1; end
             if ~exist("choice_list", "var"); choice_list = ['A', 'B', 'X']; end
             if ~exist("next_choice", "var"); next_choice = 'A'; end
+            if ~exist("epsilon", "var"); epsilon = 0.1; end
             
             obj.Behavior_Mode = behavior_mode;
             obj.Choice_List = choice_list;
-            [obj.Next_Choice, obj.Choice_Origins] = deal(next_choice);
+            obj.Epsilon = epsilon;
+            obj.Rewards = zeros(1, length(choice_list));
+            obj.Counts = zeros(1, length(choice_list));
+            obj.Next_Choice = next_choice;
+            obj.Choice_Origins = next_choice;
+            % Something that is adding up the points for the each of the
+            % choices?
         end
         
-        % Method that changes the behavior of the cpu 
-        function changeBehavior(obj)
-             switch (obj.Behavior_Mode)
-                 case 1
-                     % Code for Behavior 1 here
-                 case 2
-                     % Code for Behavior 1 here
-             end
-        end
+        function changeBehavior(obj, varargin)           %changeBehavior(obj)
+            % varargin can contain the points and choice made
+            if ~isempty(varargin)
+                points = varargin{1};
+                choice = varargin{2};
+                % Update rewards and counts
+                obj.updateRewards(choice, points);
+            end
+            %cpu1.changeBehavior(points, cpu1.Next_Choice);
+            switch obj.Behavior_Mode
+                case 1
+                    % Epsilon Greedy
+                    if rand() < obj.Epsilon
+                        % Choose a random action with probability epsilon,
+                        % aka choose a random arm
+                        obj.Next_Choice = obj.Choice_List(randi(length(obj.Choice_List)));
+                    else
+                        % Choose the best-known action with probability 1 -
+                        % epsilon, aka choose the arm with the highest
+                        % average rewards
+                        [~, best_index] = max(obj.Rewards ./ max(1, obj.Counts));
+                        obj.Next_Choice = obj.Choice_List(best_index);
+                    end
 
-        % Method that gives the cpu's responce
-        function Choice = getResponce(obj)
-             switch (obj.Behavior_Mode)
-                 case 1
-                     % Code for Behavior 1 here
-                 case 2
-                     % Code for Behavior 1 here
-             end
+                case 2
+                    % Random choice
+                    obj.Next_Choice = obj.Choice_List(randi(length(obj.Choice_List)));
+            end
         end
-    
+         % Method that gives the cpu's response
+        function Choice = getResponse(obj)
+            Choice = obj.Next_Choice;
+        end
+        % Method that updates choices and rewards
+        function updateRewards(obj, choice, reward)
+            choice_index = find(obj.Choice_List == choice);
+            obj.Rewards(choice_index) = obj.Rewards(choice_index) + reward;
+            obj.Counts(choice_index) = obj.Counts(choice_index) + 1;
+        end  
         % Resets the CPU after every block
-        function reset(obj)
-           obj.Next_Choice = obj.Choice_Origins;
+         function reset(obj)
+            obj.Next_Choice = obj.Choice_Origins;
+            obj.Rewards = zeros(1, length(obj.Choice_List));
+            obj.Counts = zeros(1, length(obj.Choice_List));
         end
     end
 end
